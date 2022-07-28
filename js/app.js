d516eb218f4bc122c90e64a700771f9f714f3f86
Xavier Hillman <xhillman13@gmail.com>
2022-07-28 18:16:32 -0400

    add local storage functionality

--- js/app.js
+++ js/app.js
@@ -14,7 +14,9 @@ Cart.prototype.addItem = function(product, quantity) {
 
 
 Cart.prototype.saveToLocalStorage = function() {
-  // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  // DONE: Fill in this instance method to save the contents of the cart to localStorage
+  let stringifiedProducts = JSON.stringify(this.items);
+  localStorage.setItem('cart', stringifiedProducts);
 };
 
 Cart.prototype.removeItem = function(item) {
