1cff5a6909e5aced94cc6af41a8af28e2cf302b4
Jose A Gonzalez <JGman13@gmail.com>
2022-07-28 11:00:55 -1000

    xavier driver

--- js/app.js
+++ js/app.js
@@ -7,8 +7,11 @@ const Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // DONE: Fill in this instance method to create a new CartItem and add it to this.items
+  let item = new CartItem(product, quantity)
+  this.items.push(item);
 };
+
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
